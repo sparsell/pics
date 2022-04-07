793c6221b0b6a6895badd5ded7de56b71cbd7ead
Sonja Parsell <sonjaparsell09@gmail.com>
2022-04-07 17:49:08 -0400

    added callback function as props

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,30 @@
 import React from 'react';
 
 class Search extends React.Component {
+    state = {term: " "};
+   
+    onFormSubmit = e => {
+        e.preventDefault();
+        // console.log(this.state.term)
+        this.props.onSubmit(this.state.term);
+    };
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label>Search here:</label>
-                            <input type="text"></input>
+                            <input 
+                                type="text" 
+                                value={this.state.term} 
+                                onChange={e => this.setState({ term: e.target.value })}
+                            />
                     </div>
                 </form>
             </div>
-        )
+        );
     }
-};
+}
 
 export default Search;
