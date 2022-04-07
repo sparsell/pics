822720a169848352168837a44580c68c1f3b28e1
Sonja Parsell <sonjaparsell09@gmail.com>
2022-04-07 13:29:25 -0400

    create App component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Search from './Search'
+
+const App = () => {
+    return (
+    <div className="ui container" style={{marginTop: '10px'}}>
+            <Search />
+        </div>
+    )
+};
+
+export default App;
