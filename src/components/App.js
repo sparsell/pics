abb1c985eef99b38a9fc043fb03288bd090f2139
Sonja Parsell <sonjaparsell09@gmail.com>
2022-04-07 17:48:43 -0400

    converted to class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Search from './Search'
 
-const App = () => {
-    return (
-    <div className="ui container" style={{marginTop: '10px'}}>
-            <Search />
-        </div>
-    )
-};
+class App extends React.Component {
+
+    onSearchSubmit(term) {
+        console.log(term)
+    }
+
+    render() {
+        return (
+        <div className="ui container" style={{marginTop: '10px'}}>
+                <Search onSubmit={ this.onSearchSubmit } />
+            </div>
+        );
+    }
+}
 
 export default App;
